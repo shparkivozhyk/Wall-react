f26b3adba32ec62ba8323b927dc8a5474456d7a4
shparkivozhyk <m.kovetskaya@gmail.com>
2017-07-04 23:46:03 +0200

    redundant routes deleted

--- src/index.js
+++ src/index.js
@@ -5,17 +5,4 @@ import { Router, Route, hashHistory, Link} from 'react-router';
 import './styles/styles.scss';
 
 
-class Whoops extends React.Component {
-    render() {
-        return (
-            <h1>This page doesn't exist</h1>
-        );
-    }
-}
-ReactDOM.render(<Router history={hashHistory}>
-                    <div>
-                        <Route path="/" component={Board}/>
-                        <Route path="*" component={Whoops}/>
-                    </div>     
-                </Router>, 
-                document.getElementById('react-container'));
+ReactDOM.render(<Board/>, document.getElementById('react-container'));
